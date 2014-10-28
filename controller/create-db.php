2a29713d322207fcd08fb367e329e1cbf53ca424
Noah Mostacero <noahmostacero@gmail.com>
2014-10-28 10:45:31 -0700

    videos 1-10 blog

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once(__DIR__ . "/../model/database.php"); //inserts code from database.php to dis one; DIR concatinates the actual directory to database
-$connection = new mysqli($host, $username, $password);
+$connection = new mysqli($host, $username, $password); //mysqli was used to build a new object
 
 if($connection->connect_error) { //if there is an error
     die("error: " . $connection->connect_error); //
