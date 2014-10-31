cd1b8cda3836289ac3d293d2c301d680c8fc1883
Noah Mostacero <noahmostacero@gmail.com>
2014-10-31 11:09:21 -0700

    Video 15 created database and fixed echoes

--- controller/create-db.php
+++ controller/create-db.php
@@ -4,7 +4,7 @@
 $connection = new mysqli($host, $username, $password); //mysqli was used to build a new object
 
     if($connection->connect_error) { //if there is an error
-    die("error: " . $connection->connect_error); //die no more code to be executed
+    die("<p>error: " . $connection->connect_error) . "</p>"; //die no more code to be executed
     }
     $exists = $connection->select_db($database); //if database exists or doesnt exist it will be stored here
     
@@ -12,11 +12,11 @@ $connection = new mysqli($host, $username, $password); //mysqli was used to buil
         $query = $connection->query("CREATE DATABASE $database"); //a query that gets applied to our connection; if there is no database create one
     
         if($query){ //if this is true then it is successful
-            echo "successfully created database: " . $database;
+            echo "<p>successfully created database: " . $database . "</p>";
         }
     }
     else {
-        echo "database has already been created"; // if the database is already created echo database here
+        echo "<p>database has already been created</p>"; // if the database is already created echo database here
     }
     $query = $connection->query("CREATE TABLE posts ( " //this creates a table so we can put info in dbase; table is called posts
             . "id int(11) NOT NULL AUTO_INCREMENT," //the id is an integer 
@@ -24,4 +24,11 @@ $connection = new mysqli($host, $username, $password); //mysqli was used to buil
             . "post text NOT NULL," // the post is text
             . "PRIMARY KEY (id))"); //theres up to 11 values in the integer so we can generate up to 1 billion ids
     // not null means id cant be empty; 
+    
+    if($query){
+        echo "<p>succesfully created table: posts</p>";
+    }
+ else {
+        echo "<p>$connection->error</p>"; //<p> works like html makes them into paragraphs
+}
     $connection->close();
