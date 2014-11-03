ddfc39bb26c736b6923b2057e8c302ef7d988d69
Noah Mostacero <noahmostacero@gmail.com>
2014-11-03 11:59:50 -0800

    Video 16 created header.php and footer.php

--- index.php
+++ index.php
@@ -1,17 +1,6 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-            require_once(__DIR__ . "/controller/create-db.php"); // inserts code from create-db.php to here
-        ?>
-    </body>
-</html>
+
+<?php
+require_once(__DIR__ . "/view/header.php"); // this insters the code from header.php
+require_once(__DIR__ . "/controller/create-db.php"); // inserts code from create-db.php to here
+require_once(__DIR__ . "/view/footer.php"); // inserts code from footer.php
+?>
