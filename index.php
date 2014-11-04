00a595c12d4a78a188b90d9153ed3fa235eafbf8
Noah Mostacero <noahmostacero@gmail.com>
2014-11-04 11:09:47 -0800

    Video 17 created the link to post.php on index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 
 <?php
-require_once(__DIR__ . "/view/header.php"); // this insters the code from header.php
-require_once(__DIR__ . "/controller/create-db.php"); // inserts code from create-db.php to here
-require_once(__DIR__ . "/view/footer.php"); // inserts code from footer.php
+    require_once(__DIR__ . "/view/header.php"); // this insters the code from header.php
+    require_once(__DIR__ . "/view/navigation.php"); // this inserts the code from header.php
+    require_once(__DIR__ . "/controller/create-db.php"); // inserts code from create-db.php to here
+    require_once(__DIR__ . "/view/footer.php"); // inserts code from footer.php
 ?>
---
+++ view/navigation.php
@@ -0,0 +1,7 @@
+<nav>
+    <ul>
+        <li>
+            <a href="../post.php">Blog Post Form</a> <!-- this leads us to post.php when clicked-->
+        </li>
+    </ul>
+</nav>
