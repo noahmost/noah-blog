5cc6f1272e9f04a1c4672edd399bc9ca4ead6efa
Noah Mostacero <noahmostacero@gmail.com>
2014-11-13 11:13:28 -0800

    Video 24 fixed posts so they now go into my database

--- controller/create-post.php
+++ controller/create-post.php
@@ -5,6 +5,12 @@
                                                   //title means this is how it knows to receive; FILER SANTIZIE makes it a string
     $post = filter_input(INPUT_POST,"post",FILTER_SANITIZE_STRING); //were saving the input that went from the post on the blog into these variables
     //the controller will recieve the info with inputpost
-    echo "<p>Title: $title</p>"; //these echo out the content saved in the title variable
-    echo "<p>Post: $post</p>"; //these echo out the content saved in the post variable
+    $query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post'"); //running a query on our database in the query were inserting in our table posts
+    if($query){
+        echo "<p>Successfully inserted post: $title</p>"; //if the query is true then echo out this string
+    }
+    else {
+       echo "<p>$connection->error</p>"; //if the query is false then echo this strings
+    }
+    
     $connection->close(); //we are closing the connection
