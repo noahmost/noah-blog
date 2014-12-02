8d7589a19e11c2da182be71d63d825d12448c017
Noah Mostacero <noahmostacero@gmail.com>
2014-12-02 11:09:20 -0800

    Finished video 37 created a registration form to post on blogg

---
+++ view/register-form.php
@@ -0,0 +1,21 @@
+<form>
+    <div>
+        <label for="email">Email: </label>
+        <!--this allows the user to input text onto the page-->
+        <input type="text" name="email" /> 
+    </div>
+    <div>
+        <label for="username">Username: </label>
+        <!--this allows the user to put text onto the page-->
+        <input type="text" name="username"/>
+    </div>
+    <div>
+        <!--this allows you to create a password and the text shows up as dots-->
+        <label for="password">Password: </label>
+        <input type="password" name="password"
+    </div>
+    <div>
+        <!--a submit button that submits your work above-->
+        <button type="submit">Submit: </button>
+    </div>
+</form>
