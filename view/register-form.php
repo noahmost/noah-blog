cd13caea932403884874b6539f720326440ccbab
Noah Mostacero <noahmostacero@gmail.com>
2014-12-04 11:28:37 -0800

    Finished video 39 created create-user and register.php

--- view/register-form.php
+++ view/register-form.php
@@ -1,4 +1,10 @@
-<form>
+<?php
+    require_once(__DIR__ . "/../model/config.php");
+?>
+
+<h1>Register</h1>
+
+<form method="post" action="<?php echo $path . "controller/create-user.php"; ?>">
     <div>
         <label for="email">Email: </label>
         <!--this allows the user to input text onto the page-->
