1c66f2a5e83379d9c39dc4d0998efa496b1b7589
Noah Mostacero <noahmostacero@gmail.com>
2014-12-04 11:04:37 -0800

    Finished video 38 created the registration so you have to register

--- controller/create-db.php
+++ controller/create-db.php
@@ -15,3 +15,19 @@
  else {
         echo "<p>" . $_SESSION["connection"]->error . "</p>"; //<p> works like html makes them into paragraphs
 }
+$query = $_SESSION["connection"]->query("CREATE TABLE users ("
+        . "id int(11) NOT NULL AUTO_INCREMENT,"
+//        NOT NULL because the username has to be said it cant be blank; varchar(30) is the username can only have 30 characters
+        . "username varchar(30) NOT NULL,"
+        . "email varchar(50) NOT NULL,"
+        . "password char(128) NOT NULL,"
+//        salt is used to create a little bit more security
+        . "salt char(128) NOT NULL,"
+        . "PRIMARY KEY (id))"); 
+//    if the query works then echo the successfully line of code if not then use else
+    if($query){
+        echo "<p>Succesfuly made a table called users: </p>";
+    }
+    else {
+        echo "<p>" . $_SESSION["connection"]->error . "</p>";
+    }
