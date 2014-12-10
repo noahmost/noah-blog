c8fd0534e482b55f62cc6ea6c6a07416d016299f
Noah Mostacero <noahmostacero@gmail.com>
2014-12-10 11:18:57 -0800

    Finished video 42 when the registration form is filled out it prints your info to the screen

--- controller/create-user.php
+++ controller/create-user.php
@@ -1,2 +1,10 @@
 <?php
-echo "made user:";
+
+//    we want to access the database object within this page
+require_once(__DIR__ . "/../model/config.php");
+$email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+//filter sanitize string means that any invalid characters that cannot be in strings are deleted
+$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
+
+echo $email . " - " . $username . " - " . $password;
