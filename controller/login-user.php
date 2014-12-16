821c1da8f18f8e168c66449a6064c3477445ea82
Noah Mostacero <noahmostacero@gmail.com>
2014-12-15 20:11:59 -0800

    Finished video 47 filled in login-user with code for the login

--- controller/login-user.php
+++ controller/login-user.php
@@ -1,2 +1,7 @@
 <?php
-    echo "Successfully logged in user";
+    require_once(__DIR__ . "/../model/config.php");
+    
+    $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+    $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
+//    this is a query where we are going to select our salt and password from users table where our username was sent in via the post
+    $query = $_SESSION["connection"]->query("SELECT salt, password FROM users WHERE username = '$username'");
