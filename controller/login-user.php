861f544a947b918b8bd6f4a315fcfff2042d5c28
Noah Mostacero <noahmostacero@gmail.com>
2014-12-15 20:31:33 -0800

    Finished video 48 checked for passwords in login and fixed an error

--- controller/login-user.php
+++ controller/login-user.php
@@ -5,3 +5,17 @@
     $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
 //    this is a query where we are going to select our salt and password from users table where our username was sent in via the post
     $query = $_SESSION["connection"]->query("SELECT salt, password FROM users WHERE username = '$username'");
+    
+    if($query->num_rows == 1){
+        $row = $query->fetch_array();
+//        check if the hashedpassword is equal to the new hashedpassword according to the salt
+        if($row["password"] === crypt($password, $row["salt"])){
+            echo "<p>Login Successful!</p>";
+        }
+        else {
+            echo "<p>Invalid username and password</p>";
+        }
+    }
+    else {
+        echo "<p>Invalid username and password</p>";
+    }
