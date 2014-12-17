df51484369abb55b4a9684258a10f77388652ed3
Noah Mostacero <noahmostacero@gmail.com>
2014-12-16 22:29:08 -0800

    Finished post page

--- controller/login-user.php
+++ controller/login-user.php
@@ -1,6 +1,6 @@
 <?php
     require_once(__DIR__ . "/../model/config.php");
-    
+    require_once (__DIR__ . "/../view/header.php");
     $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
     $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
 //    this is a query where we are going to select our salt and password from users table where our username was sent in via the post
@@ -21,3 +21,10 @@
     else {
         echo "<p>Invalid username and password</p>";
     }
+    ?>
+<html>
+    <body class="loginbody">
+        
+    </body>
+    
+</html>
--- view/forum.php
+++ view/forum.php
@@ -8,21 +8,21 @@
         die();
     }
 ?>
-<h1>CREATE BLOG POST</h1>
+<h1 class="registerbuttonthing">CREATE BLOG POST</h1>
 
 <form method="post" action="<?php echo $path . "controller/create-post.php"; ?>"> <!-- method is used to send information; action is where the info will be sent-->
-    <div>
+    <div class="registercentering">
         <label for="title">  <!--text that is displayed infront of the  text box-->
         Title:  <!-- displays title infront of the title box-->
         </label>
         <input type="text" name="title" />
     </div>
-    <div>
+    <div class="registercentering">
         <label for="post">Post: </label> <!-- displays post infront of the textarea-->
         <textarea name="post"></textarea>
         <!--a text area allows the user to type as much text as they want-->
     </div>
-    <div>
+    <div class="registercentering">
         <button type="submit">Submit: </button> <!--this is a button that should submit the post to
                                                 our database but it basically just refreshes the page-->
     </div>
