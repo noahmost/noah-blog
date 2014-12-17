e564db18486636ae19a17d818706127abb6b258e
Noah Mostacero <noahmostacero@gmail.com>
2014-12-16 22:17:15 -0800

    Finished Login page

--- view/login-form.php
+++ view/login-form.php
@@ -2,20 +2,20 @@
     require_once(__DIR__ . "/../model/config.php");
 ?>
 
-<h1>Login</h1>
+<h1 class="registerbuttonthing">Login</h1>
 
 <form method="post" action="<?php echo $path . "controller/login-user.php"?>">
-    <div>
+    <div class="registercentering">
         <label for="username">Username: </label>
         <!--this allows the user to put text onto the page-->
         <input type="text" name="username"/>
     </div>
-    <div>
+    <div class="registercentering">
         <!--this allows you to create a password and the text shows up as dots-->
         <label for="password">Password: </label>
         <input type="password" name="password"
     </div>
-    <div>
+    <div class="registercentering">
         <!--a submit button that submits your work above-->
         <button type="submit">Submit: </button>
     </div>
